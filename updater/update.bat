52ee2fee59ea15e6055c2bffd46d5f5b29e0b14f
alantr7 <49253990+alantr7@users.noreply.github.com>
2020-05-15 20:49:12 +0200

    Update update.bat

--- updater/update.bat
+++ updater/update.bat
@@ -1,7 +1,7 @@
 timeout 2
 
-del %appdata%\Alan\alan.exe
-ren %appdata%\Alan\alan.file alan.exe
+del "%appdata%\Alan\alan.exe"
+ren "%appdata%\Alan\alan.file" alan.exe
 
-del %appdata%\Alan\alan.file
-%appdata%\Alan\alan.exe
+del "%appdata%\Alan\alan.file"
+alan.exe
